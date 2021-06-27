d6543f1392c029379f0c8db09c62243e6d6221c6
André Neves <andre.neves1998@hotmail.com>
2021-06-27 17:12:28 +0100

    Project table endpoints done and functional.

--- src/main/java/pt/ua/tm/neji/nodejs-server/app/controllers/project.controller.js
+++ src/main/java/pt/ua/tm/neji/nodejs-server/app/controllers/project.controller.js
@@ -29,3 +29,127 @@ exports.create = (req, res) => {
             });
         });
 };
+
+// Retrieve all projects from the database: [GET] api/projects
+exports.findAll = (req, res) => {
+    const proj_name = req.query.proj_name;
+    // if we want to find all projects with a given name
+    var condition = proj_name ? { proj_name: { [Op.iLike]: `%${proj_name}%` } } : null;
+
+    Project.findAll({ where: condition })
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving projects."
+        });
+    });
+};
+
+// Find a single project with a name: [GET] api/projects/:name
+exports.findOne = (req, res) => {
+    const proj_name = req.params.proj_name;
+
+    Project.findByPk(proj_name)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Project with proj_name=" + proj_name
+            });
+        });
+};
+
+// Update a project by the id in the request: [PUT] api/projects/:proj_id
+exports.updateByID = (req, res) => {
+    const proj_id = req.params.proj_id;
+
+    Project.update(req.body, {
+        where: { proj_id: proj_id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Project was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Project with proj_id=${proj_id}. Maybe Project was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Project with proj_id=" + proj_id
+            });
+        });
+};
+
+// Update a project by the name in the request: [PUT] api/projects?proj_name=name
+exports.updateByName = (req, res) => {
+    const proj_name = req.query.proj_name;
+
+    Project.update(req.body, {
+        where: { proj_name: proj_name }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Project was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Project with proj_name=${proj_name}. Maybe Project was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Project with proj_name=" + proj_name
+            });
+        });
+};
+
+// Delete a Project with the specified id in the request: [DELETE] api/projects/:proj_id
+exports.deleteByID = (req, res) => {
+    const proj_id = req.params.proj_id;
+
+    Project.destroy({
+        where: { proj_id: proj_id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Project was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Project with proj_id=${proj_id}. Maybe Project was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Project with proj_id=" + proj_id
+            });
+        });
+};
+
+// Delete all Projects from the database: [DELETE] api/projects
+exports.deleteAll = (req, res) => {
+    Project.destroy({
+        where: {},
+        truncate: false
+    })
+        .then(nums => {
+            res.send({ message: `${nums} Projects were deleted successfully!` });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while removing all Projects."
+            });
+        });
+};
--- src/main/java/pt/ua/tm/neji/nodejs-server/app/routes/project.routes.js
+++ src/main/java/pt/ua/tm/neji/nodejs-server/app/routes/project.routes.js
@@ -6,6 +6,24 @@ module.exports = app => {
     // Create new project
     router.post("/", projects.create);
 
+    // Retrieve all Projects
+    router.get("/", projects.findAll);
+
+    // Retrieve a single Project with name
+    router.get("/:proj_name", projects.findOne);
+
+    // Update a Project by id
+    router.put("/:proj_id", projects.updateByID);
+
+    // Update a Project by name
+    router.put("/", projects.updateByName);
+
+    // Delete a Project by id
+    router.delete("/:proj_id", projects.deleteByID);
+
+    // Remove all Projects
+    router.delete("/", projects.deleteAll);
+
 
     app.use("/api/projects", router);
 }
